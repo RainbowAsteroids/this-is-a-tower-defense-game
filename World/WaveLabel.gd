6088ae933b4093512587644b441a2f6f6ba2e1b0
Rainbow Asteroids <rainbow@portal2d.xyz>
2023-06-22 12:43:00 -0400

    Added wave counter

---
+++ World/WaveLabel.gd
@@ -0,0 +1,4 @@
+extends Label
+
+func _on_world_wave_count_changed(value):
+	text = str(value)
--- World/world.gd
+++ World/world.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name World
 
 signal health_changed(value)
+signal wave_count_changed(value)
 signal next_round
 signal round_ended
 
@@ -10,6 +11,10 @@ signal round_ended
 	set(value):
 		health = value
 		health_changed.emit(value)
+@export var wave_count: int:
+	set(value):
+		wave_count = value
+		wave_count_changed.emit(value)
 @export var money: float = 500
 
 @export var enemy_parent: Path2D
@@ -19,12 +24,13 @@ func _ready():
 	MoneyManager.money = money
 	self.health = health
 	
-	for wave in waves:
+	for wave_index in waves.size():
+		wave_count = wave_index + 1
+		var wave = waves[wave_index]
+		round_ended.emit()
 		await next_round
 		wave.execute(enemy_parent)
 		await wave.finished
-		print("wave executed")
-		round_ended.emit()
 	
 	pass # the game is over
 
@@ -38,3 +44,7 @@ func _on_start_button_pressed():
 
 func _on_round_ended():
 	start_button.disabled = false
+
+func _on_speed_button_toggled(button_pressed):
+	var speed = 2 if button_pressed else 1
+	Engine.time_scale = speed
